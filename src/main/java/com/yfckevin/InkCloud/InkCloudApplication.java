d2d4cfd6d223e3bdbb1ae88974a7e7792c24ca52
yifan <yifanchen0914@gmail.com>
2024-09-12 21:43:18 +0800

    0912 Yfian initial commit

---
+++ src/main/java/com/yfckevin/InkCloud/InkCloudApplication.java
@@ -0,0 +1,13 @@
+package com.yfckevin.InkCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InkCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InkCloudApplication.class, args);
+	}
+
+}
